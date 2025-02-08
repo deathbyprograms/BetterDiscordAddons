00a0419b1db2aa6a472da4d76dd838327cf929c0
deathbyprograms <24487592+deathbyprograms@users.noreply.github.com>
2025-02-08 15:28:07 -0700

    Fix build script

--- scripts/copyToBd.js
+++ scripts/copyToBd.js
@@ -2,12 +2,7 @@ const fs = require("node:fs");
 const path = require("node:path");
 const os = require("node:os");
 
-if (process.argv.length < 3) {
-  console.error("Expected at least one plugin argument!");
-  process.exit(1);
-}
-
-const plugins = process.argv.slice(2);
+const plugins = fs.readdirSync("dist");
 
 for (let plugin of plugins) {
   const srcPath = path.resolve("dist", plugin, `${plugin}.plugin.js`);
@@ -17,12 +12,21 @@ for (let plugin of plugins) {
       betterDiscordLoc = path.resolve(os.homedir(), ".config");
       break;
     case "Darwin":
-      betterDiscordLoc = path.resolve(os.homedir(), "Library", "Application Support");
+      betterDiscordLoc = path.resolve(
+        os.homedir(),
+        "Library",
+        "Application Support"
+      );
       break;
     case "Windows_NT":
       betterDiscordLoc = path.resolve(process.env.APPDATA);
       break;
   }
-  const destPath = path.resolve(betterDiscordLoc, "BetterDiscord", "plugins", `${plugin}.plugin.js`)
+  const destPath = path.resolve(
+    betterDiscordLoc,
+    "BetterDiscord",
+    "plugins",
+    `${plugin}.plugin.js`
+  );
   fs.copyFileSync(srcPath, destPath);
 }
